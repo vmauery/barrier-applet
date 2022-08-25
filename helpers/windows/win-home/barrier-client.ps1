e3b884ac32b7cd27d695dc8ffae618e9a22bba58
removedporn <86824510+removedporn@users.noreply.github.com>
2022-08-25 16:59:03 +0800

    Delete helpers directory

--- helpers/windows/win-home/barrier-client.ps1
+++
@@ -1,4 +0,0 @@
-cd c:\Users\vmauery
-$wsl_ip = (wsl hostname -I).trim()
-#NO_TERM="-n -N -T"
-ssh -n -N -T -J $wsl_ip -o ExitOnForwardFailure=yes -L 24800:localhost:24800 10.1.1.5
--- helpers/windows/win-home/barrier-server.ps1
+++
@@ -1,3 +0,0 @@
-cd c:\Users\vmauery
-$wsl_ip = (wsl hostname -I).trim()
-ssh -n -N -T -J $wsl_ip -o ExitOnForwardFailure=yes -R 24800:localhost:24800 10.1.1.5
